2aa6ab57b812b862d085cafb48d6832bb597be0b
Mhd Omar Bahra <omaralbahra@gmail.com>
2019-11-26 10:40:04 +0300

    #1 db is initalized

--- database/migrations/2019_11_26_071726_create_currencies_table.php
+++ database/migrations/2019_11_26_071726_create_currencies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCurrenciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('currencies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('currency_guid')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->bigInteger('wallet');
-            $table->rememberToken();
+            $table->string('issuing_company');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('currencies');
     }
 }
---
+++ database/migrations/2019_11_26_071921_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('currency_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('transaction_guid')->unique();
+            $table->bigInteger('amount');
+            $table->bigInteger('buying_rate');
+
+            $table->foreign('currency_id')
+                ->references('id')->on('currencies');
+
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
